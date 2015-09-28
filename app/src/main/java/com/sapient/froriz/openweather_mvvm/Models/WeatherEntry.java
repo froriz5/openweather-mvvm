575b491de4da3c7037e2c33d77dc980110ac0f93
Felipe Roriz <froriz50@gmail.com>
2015-09-28 10:46:04 -0500

    Added dependencies, libraries, and plugins to implement MVVM.

---
+++ app/src/main/java/com/sapient/froriz/openweather_mvvm/Models/WeatherEntry.java
@@ -0,0 +1,12 @@
+package com.sapient.froriz.openweather_mvvm.Models;
+
+import android.databinding.BaseObservable;
+
+import java.util.Observable;
+
+/**
+ * Created by froriz on 9/28/15.
+ */
+public class WeatherEntry extends BaseObservable{
+    
+}
---
+++ app/src/test/java/MainActivityTest.java
@@ -0,0 +1,19 @@
+import com.sapient.froriz.openweather_mvvm.BuildConfig;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by froriz on 9/28/15.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class MainActivityTest {
+
+    @Before
+    public void setUp() {
+
+    }
+}
